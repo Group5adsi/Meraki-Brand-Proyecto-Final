21f4a3fbe4fa46d290f7fd2e846899a7854394e2
Yaatuesta <yaatuesta2@misena.edu.co>
2020-07-29 20:42:20 -0500

    catalogofiltro1

--- Java.js
+++ Java.js
@@ -1,14 +1,45 @@
+function cambiar() {
+    var site = document.getElementById('site-nav');
+    site
+        .classList
+        .toggle('site-nav-open');
+}
 
-      function cambiar(){
-        var site = document.getElementById('site-nav');
-            site.classList.toggle('site-nav-open');
-      }
-
-     
 //loader
 
-window.addEventListener("load", function(){
-      document.getElementById("loader").classList.toggle("loadercargado")
+window.addEventListener("load", function () {
+    document
+        .getElementById("loader")
+        .classList
+        .toggle("loadercargado")
 })
 
+//Grid
 
+const grid = new Muuri('.grid', {
+    layout: {
+        rounding: true
+    }
+});
+
+window.addEventListener('load', () => {
+    grid
+        .refreshItems()
+        .layout();
+    document
+        .getElementById('grid')
+        .classList
+        .add('imagenes-cargadas');
+
+    const enlaces = document.querySelectorAll('#categorias a');
+    enlaces.forEach((elemento) => {
+        elemento.addEventListener('click', (evento) => {
+              evento.preventDefault();
+              enlaces.forEach((enlace) => enlace.classList.remove('activo'));
+              evento.target.classList.add('activo');
+
+              const categoria = evento.target.innerHTML.toLowerCase();
+              categoria === 'todos' ? grid.filter('[data-categoria]') : grid.filter(`[data-categoria="${categoria}"]`);
+        });
+    });
+});
